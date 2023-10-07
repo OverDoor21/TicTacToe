a31b9e1a0eb94a8d7b02f22e2dea35585d5e2b85
aulin <aulin@GIGACHADUS>
2023-10-07 10:48:18 +0200

    Add project files.

---
+++ Tic-tac-toe/Graphic/ColumnGraphic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_tac_toe.Graphic
+{
+    public class ColumnGraphic
+    {
+        const int PoleHeight = 500;
+        const int PoleWidth = 500;
+
+        int NexTPalka = 30;
+    }
+}
